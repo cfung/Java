ed37eb28a4bb30e34a712d0a5fbaea17b7e8b18d
sk <chesterfung@gmail.com>
2019-08-18 22:11:16 -0700

    add constructors, getters and setters

--- P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
+++ P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -4,5 +4,20 @@ package edu.udacity.java.nano.chat;
  * WebSocket message model
  */
 public class Message {
-    // TODO: add message model.
+
+    // TODO: add message model. ENTER, CHAT, LEAVE
+    private String type;
+
+    public Message() {}
+
+    public Message(String type) {this.type = type;}
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
