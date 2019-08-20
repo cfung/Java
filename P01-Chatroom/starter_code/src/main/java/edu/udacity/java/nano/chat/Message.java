078ea7cc7376519e9bd3c208deaef08825366c96
Chester Fung <cfung@netflix.com>
2019-08-19 17:17:11 -0700

    add MessageType enums and implement serverendpoint methods

--- P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
+++ P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -6,18 +6,41 @@ package edu.udacity.java.nano.chat;
 public class Message {
 
     // TODO: add message model. ENTER, CHAT, LEAVE
-    private String type;
+
+    private String from_user;
+    private String to_user;
+    private MessageType type;
+
+    private enum MessageType {
+        ENTER,
+        CHAT,
+        LEAVE
+    }
 
     public Message() {}
 
-    public Message(String type) {this.type = type;}
-
-
-    public String getType() {
+    public MessageType getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(MessageType type) {
         this.type = type;
+    }
+
+
+    public String getFrom_user() {
+        return from_user;
+    }
+
+    public void setFrom_user(String from_user) {
+        this.from_user = from_user;
+    }
+
+    public String getTo_user() {
+        return to_user;
+    }
+
+    public void setTo_user(String to_user) {
+        this.to_user = to_user;
     }
 }
--- P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -3,9 +3,14 @@ package edu.udacity.java.nano.chat;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * WebSocket Server
@@ -15,7 +20,7 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 
 @Component
-@ServerEndpoint("/chat")
+@ServerEndpoint("/chat/{username}")
 public class WebSocketChatServer {
 
     /**
@@ -23,32 +28,55 @@ public class WebSocketChatServer {
      */
     private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
 
+    private Session session;
+
+    private static Set<WebSocketChatServer> chatServers = new CopyOnWriteArraySet<>();
+
+    private static HashMap<String, String> users = new HashMap<>();
+
     private static void sendMessageToAll(String msg) {
         //TODO: add send message method.
+        for (WebSocketChatServer server : chatServers) {
+            server.sendMessage(msg);
+        }
     }
 
     /**
+     * invoked by the container when a new WebSocket connection is initiated
      * Open connection, 1) add session, 2) add user.
      */
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session, @PathParam("username") String username) {
         //TODO: add on open connection.
+
+        this.session = session;
+        //onlineSessions.put(session);
+        chatServers.add(this);
+        users.put(session.getId(), username);
+
+        Message message = new Message();
+        message.setFrom_user(users.get(session.getId()));
     }
 
     /**
+     * receives the information from the WebSocket container when a message is sent to the endpoint
      * Send message, 1) get username and session, 2) send message to all.
      */
     @OnMessage
     public void onMessage(Session session, String jsonStr) {
         //TODO: add send message.
+        //jsonStr.(users.get(session.getId()));
+        sendMessageToAll(jsonStr);
     }
 
     /**
+     * Used to decorate a Java method that is called by the container when the WebSocket connection closes
      * Close connection, 1) remove session, 2) update user.
      */
     @OnClose
     public void onClose(Session session) {
         //TODO: add close connection.
+        chatServers.remove(this);
     }
 
     /**
@@ -57,6 +85,14 @@ public class WebSocketChatServer {
     @OnError
     public void onError(Session session, Throwable error) {
         error.printStackTrace();
+    }
+
+    private void sendMessage(String message) {
+        try {
+            this.session.getBasicRemote().sendText(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
