5d651195274ef495d25ad71b238f71f044fbefb1
Chester Fung <cfung@netflix.com>
2019-11-06 16:14:07 -0800

    modify input param

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewRepository.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ReviewRepository extends JpaRepository<Comment, Integer> {
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     List<Review> findAllByProduct(Product product);
 }
