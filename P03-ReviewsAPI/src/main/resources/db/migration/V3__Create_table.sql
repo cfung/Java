3f2f6528a8c497c3a4a7efc2c919d4d58c85e453
Chester Fung <cfung@netflix.com>
2019-12-20 22:07:02 -0800

    include flyway migration sql and configurations

---
+++ P03-ReviewsAPI/src/main/resources/db/migration/V3__Create_table.sql
@@ -0,0 +1,26 @@
+create table review
+(
+  id int auto_increment primary key,
+  title varchar (255) not null,
+  review_text varchar(10000) null,
+  created_ts timestamp default current_timestamp null,
+  recommended tinyint(1) null,
+  product_id int not null
+
+);
+
+create table product
+(
+    id          int auto_increment primary key,
+    name        varchar(255) not null,
+    description varchar(100) not null
+);
+
+create table comment
+(
+    id           int auto_increment primary key,
+    title        varchar(255)                        not null,
+    comment_text varchar(10000)                      null,
+    created_ts   timestamp default CURRENT_TIMESTAMP null,
+    review_id    int                                 not null
+);
