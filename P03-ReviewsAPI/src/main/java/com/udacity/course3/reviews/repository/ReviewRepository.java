3f9fad9fd1f54cf6eb8435e6c81edb2f23faccfe
Chester Fung <cfung@netflix.com>
2019-12-23 22:29:04 -0800

    Add method to return ReviewIds by ProductID

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewRepository.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewRepository.java
@@ -4,6 +4,7 @@ import com.udacity.course3.reviews.entities.Comment;
 import com.udacity.course3.reviews.entities.Product;
 import com.udacity.course3.reviews.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,7 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     List<Review> findAllByProduct(Product product);
+
+    @Query("Select r.id from Review r join r.product p where p.id = ?1")
+    List<Integer> findReviewIdsByProductId(Integer productId);
 }
