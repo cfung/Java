70f9637fa98ae3e8bc529fdd6c468a05ea2f82c7
sk <chesterfung@gmail.com>
2019-12-29 00:19:46 -0800

    add pricingservice tests

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,45 @@
 package com.udacity.pricing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void testGetPrice() throws Exception{
+		MvcResult result = mockMvc.perform(get("/prices/1"))
+				.andExpect(status().isOk())
+				.andReturn();
+		Assert.assertNotNull(result.getResponse().toString());
+	}
+
+	@Test
+	public void testGetAllPrices() throws Exception{
+		mockMvc.perform(get("/prices/"))
+				.andExpect(status().isOk())
+				.andReturn();
+	}
+
+
 
 }
